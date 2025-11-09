9b0b97515f46a1fe332c595e71baa2f4eef2dac2
ShepherD <mail@mail.com>
2025-11-09 18:26:52 +0300

    First Commit, Fresh start

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+class Sum
+{
+
+    public static void GetArgs(string[] args)
+    {
+        for (int i = 0; i < args.Length; i++)
+        {
+            Console.WriteLine(args[i]);
+        }
+    }
+
+    public static void Main(string[] args)
+    {
+        GetArgs(args);
+    }
+}
